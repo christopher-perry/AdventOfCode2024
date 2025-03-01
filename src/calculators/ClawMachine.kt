cf156e69923b9b1f9f35ced8d424a8b917634076
Chris Perry <cperry.software@gmail.com>
2025-02-28 17:51:28 -0800

    Day 13 pt 1 ClawMachine
    Simple implementation solution of iterating through possible combinations of buttonB down to zero, if the target is divisible by buttonA, add it to the winningCombos, at the end take the min of the winners and return it
    (nobody had multiple winners)
    
    new functions for IntPair which is really starting to get out of hand...

---
+++ src/calculators/ClawMachine.kt
@@ -0,0 +1,96 @@
+package calculators
+
+private const val A_COST = 3
+private const val B_COST = 1
+
+data class ClawMachine(
+    val buttonA: IntPair,
+    val buttonB: IntPair,
+    val prize: IntPair
+) {
+
+    data class ButtonCombo(val buttonAPresses: Int, val buttonBPresses: Int) {
+        fun cost() = A_COST * buttonAPresses + B_COST * buttonBPresses
+    }
+
+    val winningCombinations = mutableSetOf<ButtonCombo>()
+
+    init {
+        calculateWinningCombos()
+    }
+
+    private fun calculateWinningCombos() {
+        val maxBPresses : Int = min((prize / buttonB))
+        for (bPresses in maxBPresses.downTo(0)) {
+            val bValue = buttonB * bPresses
+            val target = prize - bValue
+            // To be a winning combo, target must be divisible by aX and aY
+            if (target.mod(buttonA) == 0 to 0) {
+                val aPresses = (target / buttonA)
+                // If the remainder (x,y) is not ==, it is not evenly divisible, no winning # presses here.
+                if (aPresses.first == aPresses.second) {
+                    winningCombinations.add(ButtonCombo(aPresses.first, bPresses))
+                }
+            }
+        }
+    }
+
+    fun cheapestCombo() = winningCombinations.minOfOrNull { it.cost() } ?: 0
+}
+
+class ClawMachineReader : Calculator(FILE) {
+    companion object {
+        val FILE = "claw.txt"
+    }
+
+    fun parseClawMachines(): List<ClawMachine> {
+        val clawMachines = mutableListOf<ClawMachine>()
+        val lines = parseInput()
+
+        var buttonA: IntPair? = null
+        var buttonB: IntPair? = null
+        var prize: IntPair? = null
+
+        for (line in lines) {
+            if (line.startsWith("Button A:")) {
+                buttonA = parseIntPair(line.substringAfter("Button A: "))
+            } else if (line.startsWith("Button B:")) {
+                buttonB = parseIntPair(line.substringAfter("Button B: "))
+            } else if (line.startsWith("Prize:")) {
+                prize = parseIntPair(line.substringAfter("Prize: "))
+            }
+
+            if (buttonA != null && buttonB != null && prize != null) {
+                clawMachines.add(ClawMachine(buttonA, buttonB, prize))
+                buttonA = null
+                buttonB = null
+                prize = null
+            }
+        }
+        return clawMachines
+    }
+
+    private fun parseIntPair(pointString: String): IntPair {
+        val xString = pointString.substringAfter("X").substringBefore(",").trim()
+        val yString = pointString.substringAfter("Y").trim()
+
+        val x = if (xString.startsWith("+")) {
+            xString.substringAfter("+").toInt()
+        } else {
+            xString.substringAfter("=").toInt()
+        }
+        val y = if (yString.startsWith("+")) {
+            yString.substringAfter("+").toInt()
+        } else {
+            yString.substringAfter("=").toInt()
+        }
+
+        return IntPair(x, y)
+    }
+}
+
+fun main() {
+    val clawMachines = ClawMachineReader().parseClawMachines()
+    clawMachines.forEach { println("$it: ${it.winningCombinations}") }
+    println(clawMachines.sumOf { it.cheapestCombo() })
+}
--- src/calculators/MapCalculator.kt
+++ src/calculators/MapCalculator.kt
@@ -1,4 +1,7 @@
 package calculators
+
+import kotlin.math.min
+
 typealias IntPair = Pair<Int, Int>
 
 /**
@@ -30,4 +33,20 @@ operator fun IntPair.plus(other: IntPair): IntPair {
 
 operator fun IntPair.minus(other: IntPair): IntPair {
     return this.first - other.first to this.second - other.second
+}
+
+operator fun IntPair.times(other: Int): IntPair {
+    return this.first * other to this.second * other
+}
+
+operator fun IntPair.div(other: IntPair): IntPair {
+    return this.first / other.first to this.second / other.second
+}
+
+fun min(pair: Pair<Int, Int>) = min(pair.first, pair.second)
+
+fun IntPair.mod(other: IntPair): IntPair {
+    val newFirst = if (other.first == 0) 0 else this.first.mod(other.first) 
+    val newSecond = if (other.second == 0) 0 else this.second.mod(other.second) 
+    return newFirst to newSecond
 }
